ad9ab9f3831829dd7dd776828a43336aca6992a6
xandr <xandro.diels@gmail.com>
2025-09-23 11:43:43 +0200

    rewrote factcheck and promote to use / functionality

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import random
-
 import discord
-from discord.ext import commands
+from attr.filters import exclude
+from discord import app_commands
 from database import ModerationDatabase
 import json
 
@@ -30,41 +30,94 @@ intents.message_content = True
 intents.guilds = True
 intents.members = True  # Needed to access member roles
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+# !!!!!! FOR TESTING, CHANGE WHEN UPLOADING TO ORANGEPI !!!!
+GUILD_ID = 1401612201596555285
+class MyClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.tree = app_commands.CommandTree(self)
+    async def setup_hook(self):
+        guild = discord.Object(id=GUILD_ID)
+        self.tree.copy_global_to(guild=guild)
+        await self.tree.sync(guild=guild)
 
-async def admin_check(ctx):
-    author = ctx.author
-    role_names = [role.name for role in author.roles]
-    user_level = [role for role in AUTHORIZED_ROLES if role in role_names]
-    db.remove_expired_strikes()
-    if user_level:
+# Subclassing commands.Bot to register slash commands
+# class MyClient(discord.Client):
+#     def __init__(self):
+#         super().__init__(intents=discord.Intents.default())
+#         self.tree = app_commands.CommandTree(self)
+#     async def setup_hook(self):
+#         # Sync commands with Discord
+#         await self.tree.sync()
+client = MyClient()
+
+
+# Admin check, check if user has any roles in the authorized roles list
+async def admin_check(interaction):
+    author = interaction.user
+    # Check if they have a role named "Admin"
+    if any(role.name in AUTHORIZED_ROLES for role in author.roles):
         return True
     else:
-        await ctx.send("You are not authorized to use this command.")
+        await interaction.response.send_message("You are not authorized to use this command")
         return False
 
+# Silly fact check commands, checks if user has an authorized role and response positively if they do.
+@client.tree.command(name="factcheck",description="The silly factcheck, totally fair and all knowing.")
+async def factcheck(interaction: discord.Interaction):
+    author = interaction.user
+    # Check if they have a role named "Admin"
+    if any(role.name in AUTHORIZED_ROLES for role in author.roles):
+        await interaction.response.send_message(random.choice(SILLY_FACT_CHECK_POSITIVE))
+    else:
+        await interaction.response.send_message(random.choice(SILLY_FACT_CHECK_NEGATIVE))
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user}')
+# Start of the promote command
 
-# Actually commands
-# doctorate @user nick
-@bot.command()
-async def doc(ctx):
-    if await admin_check(ctx):
-        # get the roles for new people
-        role2 = discord.utils.get(ctx.guild.roles, name="Waiting for BCT")
-        target = ctx.message.mentions[0]
-        content = ctx.message.content
-        split_content = content.split()
-        new_nick = (" " + split_content[2])
-        await target.add_roles(role2)
-        await target.edit(nick=new_nick)
-        await ctx.send(new_nick + " has been drafted")
-# -----------------------------------
-async def promotion(user_ranks, target, branch, prefix_type,ctx):
-    current_index = branch.index(user_ranks[0])
+@client.tree.command(name="promote",description="Promotes users mentioned in the arguments.")
+async def promote(
+        interaction: discord.Interaction,
+        target1: discord.Member ,
+        target2: discord.Member = None,
+        target3: discord.Member = None,
+        target4: discord.Member = None,
+        target5: discord.Member = None,
+        target6: discord.Member = None,
+        target7: discord.Member = None,
+        target8: discord.Member = None,
+        target9: discord.Member = None,
+        target10: discord.Member = None
+):
+    await interaction.response.defer()
+    targets_unfiltered = [target1,target2,target3,target4,target5,target6,target7,target7,target8,target9,target10]
+    targets = [t for t in targets_unfiltered if t is not None]
+    if await admin_check(interaction):
+        for target in targets:
+            role_names = [role.name for role in target.roles if role.name != "@everyone"]
+            user_ranks_ground = [role for role in GROUND_ROLE_HIERARCHY if role in role_names]
+            nco_ranks = [role for role in NCO_ROLE_HIERARCHY if role in role_names]
+            air_ranks = [role for role in AIR_ROLE_HIERARCHY if role in role_names]
+            armor_ranks = [role for role in ARMOR_ROLE_HIERARCHY if role in role_names]
+            if user_ranks_ground:
+                branch = GROUND_ROLE_HIERARCHY
+                prefix_type = GROUND_ROLE_PREFIX
+                await promotion(user_ranks_ground, target, branch,prefix_type,interaction)
+            if nco_ranks:
+                branch = NCO_ROLE_HIERARCHY
+                prefix_type = NCO_ROLE_PREFIX
+                await promotion(nco_ranks, target, branch, prefix_type,interaction)
+            if air_ranks:
+                branch = AIR_ROLE_HIERARCHY
+                prefix_type = AIR_ROLE_PREFIX
+                await promotion(air_ranks, target, branch, prefix_type,interaction)
+            if armor_ranks:
+                branch = ARMOR_ROLE_HIERARCHY
+                prefix_type = ARMOR_ROLE_PREFIX
+                await promotion(armor_ranks, target, branch, prefix_type,interaction)
+        await interaction.followup.send("I am done :)")
+
+async def promotion(ranks, target, branch, prefix_type, interaction):
+    current_index = branch.index(ranks[0])
     next_index = current_index + 1
     if next_index < len(branch):
         # Get the name of the current rank and next rank, then search up the corresponding role
@@ -72,8 +125,7 @@ async def promotion(user_ranks, target, branch, prefix_type,ctx):
         next_rank_name = branch[next_index]
         current_role = discord.utils.get(target.guild.roles, name=current_rank_name)
         next_role = discord.utils.get(target.guild.roles, name=next_rank_name)
-        author = ctx.message.author
-        author_name = author.display_name
+        author = interaction.user
         # Add the next role (promotion) and remove the cleanup.
         await target.add_roles(next_role)
         await target.remove_roles(current_role)
@@ -86,132 +138,12 @@ async def promotion(user_ranks, target, branch, prefix_type,ctx):
         # nickname_parts[1] User
         new_nickname = new_prefix + " " + nickname_parts[1]
         await target.edit(nick=new_nickname)
-        await ctx.send("Congrats " + target.mention + " you got promoted from " + str(current_rank_name) + " to " + str(next_rank_name) + " by " + author_name)
+        await interaction.channel.send("Congrats " + target.mention + " you got promoted from " + str(current_rank_name) + " to " + str(next_rank_name) + " by " + author.mention)
     else:
-        await ctx.send(target.display_name + " is already the highest rank in his/her branch")
+        await interaction.channel.send(target.display_name + " is already the highest rank in his/her branch")
 
-@bot.command()
-# the !promote command
-async def promote(ctx):
-    # First check to see if the sender has enough privilege to execute command
-    if await admin_check(ctx):
-        # Check who the user mentioned, that is the target.
-        targets = ctx.message.mentions
-        # If the target is in ground branch call promotion function with branch
-        for target in targets:
-            # Filter for roles in each branch separately
-            # Get the roles of the suer.
-            role_names = [role.name for role in target.roles]
-            user_ranks_ground = [role for role in GROUND_ROLE_HIERARCHY if role in role_names]
-            nco_ranks = [role for role in NCO_ROLE_HIERARCHY if role in role_names]
-            air_ranks = [role for role in AIR_ROLE_HIERARCHY if role in role_names]
-            armor_ranks = [role for role in ARMOR_ROLE_HIERARCHY if role in role_names]
-            if user_ranks_ground:
-                branch = GROUND_ROLE_HIERARCHY
-                prefix_type = GROUND_ROLE_PREFIX
-                await promotion(user_ranks_ground, target, branch,prefix_type,ctx)
-            if nco_ranks:
-                branch = NCO_ROLE_HIERARCHY
-                prefix_type = NCO_ROLE_PREFIX
-                await promotion(nco_ranks, target, branch, prefix_type,ctx)
-            if air_ranks:
-                branch = AIR_ROLE_HIERARCHY
-                prefix_type = AIR_ROLE_PREFIX
-                await promotion(air_ranks, target, branch, prefix_type,ctx)
-            if armor_ranks:
-                branch = ARMOR_ROLE_HIERARCHY
-                prefix_type = ARMOR_ROLE_PREFIX
-                await promotion(armor_ranks, target, branch, prefix_type,ctx)
-        await ctx.message.delete()
-#         ---------------------------------------------
-@bot.command()
-# the silly fact check command
-async def factcheck(ctx):
-    author = ctx.author
-    role_names = [role.name for role in author.roles]
-    user_level = [role for role in AUTHORIZED_ROLES if role in role_names]
-    db.remove_expired_strikes()
-    if user_level:
-        await ctx.send(random.choice(SILLY_FACT_CHECK_POSITIVE))
-    else:
-        await ctx.send(random.choice(SILLY_FACT_CHECK_NEGATIVE))
-#           ------------------------------------------
 
-@bot.command()
-#The strike command
-# Check if user is admin, if yes continue
-# Get he user_id from each mentioned person and send a strike alongside the expire date(how long until expires) to the db
-async def strike(ctx):
-    if await admin_check(ctx):
-        targets = ctx.message.mentions
-        for target in targets:
-            target_user_id = target.id
-            db.add_strike(target_user_id,15778463)
-            if db.get_strikes(target_user_id) < 3:
-                await ctx.send(f"{target.display_name} has been struck and now has {db.get_strikes(target_user_id)} strikes.  ")
-            else:
-                await ctx.send(f"{target.display_name} has three or more strikes and should be banned, get em boys.")
-@bot.command()
-async def remove_strike(ctx):
-    if await admin_check(ctx):
-        targets = ctx.message.mentions
-        for target in targets:
-            target_user_id = target.id
-            if db.get_strikes(target_user_id) > 0:
-                db.remove_strike(target_user_id)
-                await ctx.send(f"{target.display_name} now has {db.get_strikes(target_user_id)} strikes.")
-            else:
-                await ctx.send(f"{target.display_name} has no strikes to remove.")
 
-@bot.command()
-# warning command
-# Check for admin privilege, if admin continue
-# for all mention user get user_id and put a warning in the db
-async def warn(ctx):
-    if await admin_check(ctx):
-        targets = ctx.message.mentions
-        for target in targets:
-            target_user_id = target.id
-            if db.get_warnings(target_user_id) < 3:
-                db.add_warning(target_user_id,)
-                await ctx.send(f"{target.display_name} has been warned.")
-            else:
-                db.reset_warnings(target_user_id)
-                db.add_strike(target_user_id,15778463)
-                await ctx.send(f"{target.display_name} has 3 or more warnings and has been struck. Now he has {db.get_strikes(target_user_id)} strikes and {db.get_warnings(target_user_id)} warns.")
 
-@bot.command()
-async def remove_warn(ctx):
-    if await admin_check(ctx):
-        targets = ctx.message.mentions
-        for target in targets:
-            target_user_id = target.id
-            if db.get_warnings(target_user_id) > 0:
-                db.remove_warning(target_user_id)
-                await ctx.send(f"{target.display_name} now has {db.get_warnings(target_user_id)} warnings.")
-            else:
-                await ctx.send(f"{target.display_name} has no warnings to remove.")
 
-@bot.command()
-async def info(ctx):
-    targets = ctx.message.mentions
-    for target in targets:
-        strikes = db.get_strikes(target.id)
-        warnings = db.get_warnings(target.id)
-        await ctx.send(f"{target.display_name} has {strikes} strikes and {warnings} warnings.")
-
-@bot.command()
-async def reset(ctx):
-    if await admin_check(ctx):
-        targets = ctx.message.mentions
-        for target in targets:
-            target_user_id = target.id
-            db.reset_strikes(target_user_id)
-            db.reset_warnings(target.id)
-            await ctx.send(f"{target.display_name} has been reset.")
-
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.reply(f"An error occurred:\n```{error}```")
-
-bot.run('MTQwOTEyNTgxMDM3MTI5NzMxNQ.GEHqA4.OaPQOqb6lT6qAIaGenU7ChjuWZ8uNNYLaCjiU8')
+client.run('MTQwOTEyNTgxMDM3MTI5NzMxNQ.GEHqA4.OaPQOqb6lT6qAIaGenU7ChjuWZ8uNNYLaCjiU8')
